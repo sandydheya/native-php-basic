664b3d48bae48501c873b9f777cfc6dfffc5491d
sandydheya <62000027+sandydheya@users.noreply.github.com>
2020-03-25 13:26:18 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php
+
+// Pemberian Variabel
+$umur = 22;
+
+	echo "Selamat Datang Kembali, Sandy!<br><br>";
+	
+
+// Percabangan If Else
+	echo "Lagi mau coba if else<br>";
+	if($umur < 30){
+		echo  "Umur kamu sekarang $umur";
+	} else {
+		echo "Umur kamu sekarang $umur tahun";
+	}
+	echo "<br>";
+// Perulangan for
+	echo "<br>Lagi coba perulangan for<br>";
+	for ($i = 0; $i < 5; $i++){
+		echo "string ke-$i<br>";
+	}
+
+// List
+	echo "<br>Find Me on";
+	echo "<li><a href = www.facebook.com > Facebook ";
+	echo "<li><a href = www.twitter.com > Twitter : ";
+	echo "<li><a href = www.instagram.com > Instagram : ";
